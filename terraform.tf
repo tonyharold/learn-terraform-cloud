50ae9e4f318f2f50756a2a15311e7f9bdeed4961
Tony Harold <tonyharoldnz@gmail.com>
2023-01-24 13:48:48 +1300

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,14 @@
 terraform {
 
+/*
   cloud {
-    organization = "organization-name"
+    organization = "tonyharold"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
+*/
 
   required_providers {
     aws = {
