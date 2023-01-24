677633ce42fdfe51ba6c803f4da158efb82be494
Tony Harold <tonyharoldnz@gmail.com>
2023-01-24 15:17:23 +1300

    Push a auth0_Connection

---
+++ auth_SAMLP_LM_AsiaPacific.tf
@@ -0,0 +1,13 @@
+resource "auth0_connection" "saml_lmap" {
+  name     = "someConnectionName"
+  strategy = "samlp"
+  options = {
+    fields_map = {
+      given_name       = "fn"
+      family_name      = "sn"
+      name             = "cn"
+      email            = "mail"
+      products_allowed = "isMemberOf"
+    }
+  }
+}
